d94dd6d3b92928d23f48d15e0771031763c9110a
Tyler <coolty96@icloud.com>
2021-03-10 21:37:19 -0700

    MVC pattern with dynamic rendering

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
+app.use('/main', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/rooms', roomsRouter);
